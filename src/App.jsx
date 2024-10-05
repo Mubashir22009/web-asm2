f4160301082f315ca3de7470755ff12063e9a67f
Mubashir22009 <bscs22009@itu.edu.pk>
2024-10-05 18:33:53 +0500

    package lock

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AdminCard from './components/AdminCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <AdminCard
+        title="Total Admins"
+        value="5"
+        percentage="2.3%"
+        sinceWhen="since last month"
+      ></AdminCard>
     </>
   )
 }
